1dc751b8499a055ff11bd95bd66f91c1cb076763
Steven Deobald <steven@deobald.ca>
2013-11-12 17:29:53 +0530

    [sd] fixed recursive call to get-connection.

--- src/regarde/models/migration.clj
+++ src/regarde/models/migration.clj
@@ -4,37 +4,25 @@
 
 (defn create-exercises [db]
   (j/db-do-commands db (str "create table exercises ("
-                            "id serial PRIMARY KEY"
-                            "name varchar NOT NULL"
-                            "email varchar NOT NULL"
-                            "created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"))
-  ;; (j/create-table :exercises
-  ;;                   [:id :serial "PRIMARY KEY"]
-  ;;                   [:name :varchar "NOT NULL"]
-  ;;                   [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])
-  )
+                            " id serial PRIMARY KEY,"
+                            " name varchar NOT NULL,"
+                            " email varchar NOT NULL,"
+                            " created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP);")))
 
 (defn create-users [db]
   (j/db-do-commands db (str "create table users ("
-                         "id serial PRIMARY KEY"
-                         "name varchar NOT NULL"
-                         "email varchar NOT NULL"
-                         "created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP);"))
-  ;; (j/create-table :users
-  ;;                   [:id :serial "PRIMARY KEY"]
-  ;;                   [:name :varchar "NOT NULL"]
-  ;;                   [:email :varchar "NOT NULL"]
-  ;;                   [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])
-  )
+                         " id serial PRIMARY KEY,"
+                         " name varchar NOT NULL,"
+                         " email varchar NOT NULL,"
+                         " created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP);")))
 
 (defn drop-tables [db]
-  (j/db-do-commands db "drop table users;")
-  (j/db-do-commands db "drop table exercises;"))
+  (j/db-do-commands db "drop table if exists users;")
+  (j/db-do-commands db "drop table if exists exercises;"))
 
-(defn -main []
-  (println (str "hi? " (System/getenv "REGARDE_DATABASE_URL")))
-  (let [db-spec {:connection-uri (System/getenv "REGARDE_DATABASE_URL")}
-        db (j/get-connection db-spec)]
+(defn -main [] 
+  (let [db-spec (System/getenv "REGARDE_DATABASE_URL")
+        db {:connection (j/get-connection db-spec)}]
     (println "Deleting tables...")
     (drop-tables db)
     (println "Creating database structure...")
@@ -45,4 +33,4 @@
     (create-users db)
     (println "Created users...")
     (println " done")
-    (.close db)))
+    (.close (:connection db))))
